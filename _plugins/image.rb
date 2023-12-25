838c874cf994031f7ddabb19763f0efdb476648c
Rafisto <1.rafal.wlodarczyk@gmail.com>
2023-12-25 23:09:31 +0100

    Homepagefix

--- _plugins/image.rb
+++ _plugins/image.rb
@@ -1,7 +1,7 @@
 module JekyllPluginHooks
     def lazy_load_image
       Proc.new do |webpage| 
-        webpage.output.gsub!(%r{<img (.*?)>}) { |match| '<div class="lazy-image-wrapper">' + match + '</div>' }
+        # webpage.output.gsub!(%r{<img (.*?)>}) { |match| '<div class="lazy-image-wrapper">' + match + '</div>' }
         webpage.output.gsub!('<img ','<img loading="lazy"')
       end
     end
